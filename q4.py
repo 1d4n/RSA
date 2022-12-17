b390fe9e9a3ec4c380ebbeefb14675677361fc1e
idan <idan9112@gmail.com>
2022-12-17 20:27:03 +0200

    Update solution to riddle 4.

--- q4.py
+++ q4.py
@@ -1,8 +1,16 @@
-from number_theory_functions import is_prime, modular_inverse
+from number_theory_functions import modular_exponent
 
 e = 11
 N = 991
+E = lambda x: modular_exponent(x, e, N)
 
-print(f"{is_prime(N) = }")
-phi_N = 990
-print(f"Inverse of {e} in U_{phi_N} is: {modular_inverse(e, phi_N)}")
+d = dict()
+for a in range(N):
+    img = E(a)
+    if img in d:
+        print(f"E is not injective. E({a}) = E({d[img]}) = {img}")
+        break
+    else:
+        d[img] = a
+else:  # if we didn't reach the break statement
+    print("E is injective.")
